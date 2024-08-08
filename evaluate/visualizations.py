fc83c990593ec28f1a1968976e18512da11f6737
CamaroTheBOSS <plessico@wp.pl>
2024-08-08 10:46:26 +0200

    Added loss visualization loss_vsr(loss_vc) function

--- evaluate/visualizations.py
+++ evaluate/visualizations.py
@@ -1,8 +1,12 @@
 import glob
 import os
+import wandb
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.collections as mcoll
+import matplotlib.path as mpath
 
 from LibMTL.utils import set_random_seed
 from datasets import UVGDataset
@@ -11,6 +15,7 @@ from plots import _validate_task
 from loader.model_loader import load_model
 from utils import to_cv2
 
+wandb.require("core")
 
 def validate_box(box, H, W):
     y1 = max(box[1], 0)
@@ -56,6 +61,71 @@ def mosaic(task, model_roots, example, gen_mask=None, box=(0, 0, 100, 100), fram
     cv2.imwrite(os.path.join(save_root, f"{task}_original.png"), original)
 
 
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+def colorline(
+    x, y, z=None, cmap=plt.get_cmap('copper'), norm=plt.Normalize(0.0, 1.0),
+        linewidth=3, alpha=1.0):
+    """
+    http://nbviewer.ipython.org/github/dpsanders/matplotlib-examples/blob/master/colorline.ipynb
+    http://matplotlib.org/examples/pylab_examples/multicolored_line.html
+    Plot a colored line with coordinates x and y
+    Optionally specify colors in the array z
+    Optionally specify a colormap, a norm function and a line width
+    """
+
+    # Default colors equally spaced on [0,1]:
+    if z is None:
+        z = np.linspace(0.0, 1.0, len(x))
+
+    # Special case if a single number:
+    if not hasattr(z, "__iter__"):  # to check for numerical input -- this is a hack
+        z = np.array([z])
+
+    z = np.asarray(z)
+
+    segments = make_segments(x, y)
+    lc = mcoll.LineCollection(segments, array=z, cmap=cmap, norm=norm,
+                              linewidth=linewidth, alpha=alpha)
+
+    ax = plt.gca()
+    ax.add_collection(lc)
+
+    return lc
+
+
+def make_segments(x, y):
+    """
+    Create list of line segments from x and y coordinates, in the correct format
+    for LineCollection: an array of the form numlines x (points per line) x 2 (x
+    and y) array
+    """
+
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    return segments
+
+
+def loss_plot(run_string):
+    api = wandb.Api()
+    run = api.run(run_string)
+    metrics_dataframe = run.scan_history(keys=["train_vc_loss", "train_vsr_loss"])
+    vc_loss = moving_average(np.array([row["train_vc_loss"] for row in metrics_dataframe]), 10)
+    vsr_loss = moving_average(np.array([row["train_vsr_loss"] for row in metrics_dataframe]), 10)
+    z = np.power(np.linspace(0.0005, 1.0, len(vc_loss)), 1/8)
+    colorline(vc_loss, vsr_loss, z=z, cmap=plt.get_cmap('YlOrRd'))
+    plt.xlim([np.min(vc_loss), np.max(vc_loss)])
+    plt.ylim([np.min(vsr_loss), np.max(vsr_loss)])
+    plt.xlabel("vc loss")
+    plt.ylabel("vsr loss")
+    plt.show()
+
+
 if __name__ == "__main__":
     set_random_seed(777)
-    mosaic("vsr", ["../weights/isric 1024"], 4, save_root="../weights", box=(300, 100, 100, 100))
+    loss_plot("camarotheboss/VSRVC/zvg2v1fl")
+    # mosaic("vsr", ["../weights/isric 1024"], 4, save_root="../weights", box=(300, 100, 100, 100))
