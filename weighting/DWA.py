Fixed gradient logging logic. Now it should work

weighting/DWA.py
@@ -25,7 +25,7 @@ class DWA(AbsWeighting):
             batch_weight = self.task_num*F.softmax(w_i/T, dim=-1)
         else:
             batch_weight = torch.ones_like(losses).to(self.device)
-        if kwargs["log_grads"] is not None:
+        if kwargs["log_grads"]:
             losses = torch.mul(losses, batch_weight)
             self._compute_grad_dim()
             grads = self._compute_grad(losses, mode="backward")

weighting/GLS.py
@@ -16,7 +16,7 @@ class GLS(AbsWeighting):
         super(GLS, self).__init__()
         
     def backward(self, losses, **kwargs):
-        if kwargs["log_grads"] is not None:
+        if kwargs["log_grads"]:
             raise AttributeError("log_grads not supported in GLS weighting method")
         loss = torch.pow(losses.prod(), 1./self.task_num)
         loss.backward()

weighting/RLW.py
@@ -18,7 +18,7 @@ class RLW(AbsWeighting):
         
     def backward(self, losses, **kwargs):
         batch_weight = F.softmax(torch.randn(self.task_num), dim=-1).to(self.device)
-        if kwargs["log_grads"] is not None:
+        if kwargs["log_grads"]:
             losses = torch.mul(losses, batch_weight)
             self._compute_grad_dim()
             grads = self._compute_grad(losses, mode="backward")

weighting/UW.py
@@ -19,7 +19,7 @@ class UW(AbsWeighting):
         self.loss_scale = nn.Parameter(torch.tensor([-0.5]*self.task_num, device=self.device))
         
     def backward(self, losses, **kwargs):
-        if kwargs["log_grads"] is not None:
+        if kwargs["log_grads"]:
             losses = (losses / (2 * self.loss_scale.exp()) + self.loss_scale / 2)
             self._compute_grad_dim()
             grads = self._compute_grad(losses, mode="backward")